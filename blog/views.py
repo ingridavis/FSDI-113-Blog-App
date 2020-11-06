c35d95775a65f351c140c1fc77c302d9b0320dd9
root <root@Sam_Asus.localdomain>
2020-11-05 21:38:29 -0800

    update

--- blog/views.py
+++ blog/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
+
 from .models import Post
 
 class BlogListView(ListView):
