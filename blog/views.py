e1dde9f120038079c408d3ac603a87e99e29fb0b
root <root@Sam_Asus.localdomain>
2020-11-05 21:09:30 -0800

    initial

--- blog/views.py
+++ blog/views.py
@@ -31,7 +31,3 @@ class BlogDeleteView(DeleteView):
     template_name = 'post_delete.html'
     success_url = reverse_lazy('home')
 
-class SignUpView(generic.CreateView):
-    form_class= UserCreationForm
-    success_url = reverse_lazy('login')
-    template_name = 'registration/signup.html'
